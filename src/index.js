a246646351a22d306ddf4836580b9fa4a5db063b
Manuel De la Penna <manueldelapenna@gmail.com>
2019-11-19 10:38:41 -0300

    acomodo import de themes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { fade, makeStyles, createStyles, useThemes } from '@material-ui/core/styles';
-export const MaterialStyles = { fade, makeStyles, createStyles, useThemes };
+import { fade, makeStyles, createStyles, useTheme } from '@material-ui/core/styles';
+export const MaterialStyles = { fade, makeStyles, createStyles, useTheme };
 
 if(typeof window == "object"){
     window.MaterialStyles = MaterialStyles;
