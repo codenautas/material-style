349690fa1199ce0321968209c70561eee1fadfee
Manuel De la Penna <manueldelapenna@gmail.com>
2019-12-17 11:15:16 -0300

    agrego withStyles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { fade, makeStyles, createStyles, useTheme } from '@material-ui/core/styles';
-export const MaterialStyles = { fade, makeStyles, createStyles, useTheme };
+import { fade, makeStyles, createStyles, useTheme, withStyles } from '@material-ui/core/styles';
+export const MaterialStyles = { fade, makeStyles, createStyles, useTheme, withStyles };
 
 if(typeof window == "object"){
     window.MaterialStyles = MaterialStyles;
