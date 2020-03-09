9610946e11016b11ca09e6d4d0f46e693a8c963a
manueldelapenna <manueldelapenna@gmail.com>
2020-03-09 11:51:32 -0300

    agrego Autocomplete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { fade, makeStyles, createStyles, useTheme, withStyles } from '@material-ui/core/styles';
-export const MaterialStyles = { fade, makeStyles, createStyles, useTheme, withStyles };
+import { Autocomplete } from "@material-ui/lab"
+export const MaterialStyles = { fade, makeStyles, createStyles, useTheme, withStyles, Autocomplete };
 
 if(typeof window == "object"){
     window.MaterialStyles = MaterialStyles;
